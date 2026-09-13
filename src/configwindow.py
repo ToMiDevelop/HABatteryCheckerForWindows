c6d87d69904213608bffa52537d1a789086878ed
ToMiDevelop <ToMiDevelop@outlook.com>
2026-09-13 22:51:40 +0200

    Minor changes

--- src/configwindow.py
+++ src/configwindow.py
@@ -86,7 +86,7 @@ class ConfigWindow(ctk.CTk):
         self.title(f"{configWindow["plTitle"]}")
 
         # window geometry
-        self.geometry("650x745")
+        self.geometry("650x755")
         self.resizable(False, False)
 
         # --- Main Header Section ---
